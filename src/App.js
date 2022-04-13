c6cca294023c4635d18abed2e07f4745a2113d03
Mia Sonv <thswlgml9598@gmail.com>
2022-04-13 21:59:02 +1000

    tile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Select } from 'antd';
+import {useEffect, useState} from "react";
+
+const { Option } = Select;
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [level,setLevel] = useState('easy');
+
+    function handleChange(value) {
+        setLevel(value);
+    }
+
+    const Tile = ({color}) => {
+        return (
+            <div style={{
+                width: 20,
+                height: 20,
+                backgroundColor: color,
+                float: 'left',
+                border: '1px solid gray'
+            }}></div>
+        )
+    }
+
+    //column
+    const TileLine = () => {
+
+        const [boxList, setBoxList] = useState([]);
+
+        useEffect(()=>{
+
+            let boxlist = []
+            let column = 10;
+            switch (level) {
+                case 'easy' :
+                    column = 10;
+                    break;
+                case 'normal' :
+                     column = 20;
+                    break;
+                case 'hard' :
+                     column = 30;
+                     break;
+            }
+
+            for(var i = 0; i < column; i++){
+               boxlist.push(<Tile color={i === 12 ? 'red' : 'black'}/>)
+            }
+            setBoxList(boxlist);
+
+        },[level])
+
+        return (
+            <div style={{height: 22}}>
+                {boxList}
+            </div>
+        )
+    }
+    return (
+        <>
+            <Select
+              value={level}
+                style={{ width: 120 }}
+                onChange={handleChange}
+            >
+                <Option value="easy">easy</Option>
+                <Option value="normal">normal</Option>
+                <Option value="hard">hard</Option>
+            </Select>
+
+
+            {/*row*/}
+            <TileLine/>
+            <TileLine/>
+            <TileLine/>
+            <TileLine/>
+            <TileLine/>
+
+        </>
+    )
 }
 
 export default App;
